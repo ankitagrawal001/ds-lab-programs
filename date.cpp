c8958212abacc59e77c1385d98067dd0b8f9a68f
Melrick Mascarenhas <melrickjm@gmail.com>
2015-11-05 23:00:32 +0530

    rectified errors

--- date.cpp
+++ date.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int leapm[]={0,31,29,31,30,31,30,31,31,30,31,30,31};
 int nleapm[]={0,31,29,31,30,31,30,31,31,30,31,30,31};
-
+int leapyear=0;
 class date{
 		public:
-			int dd,mm,yy,leapyear;
+			int dd,mm,yy;
 
 			void getData(){
-					int valid=0;leapyear=0;
+					int valid=0;
 					do{
 						cout<<"enter year:";
 						cin>>yy;
@@ -31,11 +31,13 @@ class date{
 
 			date operator + (int nod){
 				nod+=dd;
+				dd=0;
+			
 				while((leapyear==1 && nod>leapm[mm])||(leapyear==0 && nod>nleapm[mm])){
 					if(leapyear==1)
-						nod-=leapm[mm];
+						nod=nod-leapm[mm];
 					else
-						nod-=nleapm[mm];
+						nod=nod-leapm[mm];
 					mm++;
 					if(mm>12)
 					{
@@ -58,25 +60,24 @@ class date{
 	int operator -(date d1, date d2){
 		int count=0;
 		while(d1.mm!=d2.mm || d1.yy != d2.yy){
-			if(d2.leapyear==1)
+			if(leapyear==1)
 				count+=leapm[d2.mm];
 			else
-				count+-nleapm[d2.mm];
+				count+=nleapm[d2.mm];
 			d2.mm++;
 			if(d2.mm>12){
 				d2.mm=1;
 				d2.yy++;
 				if(d2.yy%4==0)
-					d2.leapyear=1;
+					leapyear=1;
 					}
 		}//end while
-
 		count+=d1.dd-d2.dd;
 		return count;
 	}
 
 	ostream &operator << (ostream &out , date ob){
-		out<<ob.dd<<"/"<<ob.mm<<"/"<<ob.yy;
+		out<<ob.dd<<"/"<<ob.mm<<"/"<<ob.yy<<endl;
 		return out;
 		}
 
@@ -84,14 +85,20 @@ class date{
 	{
 			date d1,d2;
 			int nod,res;
+			cout<<"INPUT CONDITION: date1 >= date 2\n";
 			cout<<"enter date1:";
 			d1.getData();
 			cout<<"enter date2:";
 			d2.getData();
 			res=d1-d2;
-			cout<<res;
-			cout<<"enter day to add:";
+			cout<<"\nThe difference between the dates:"<<res;
+			cout<<"\nenter day to add:";
 			cin>>nod;
 			cout<<d1+nod;
 			return 0;
 	}	
+	
+	/* There are some minor errors in this program ... well rectify it soon. 
+	
+	
+	Author - Melrick  */
